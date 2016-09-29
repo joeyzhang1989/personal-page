5e95b5bf53feab091192abf43dedf742c057923c
chenglong zhang <chenglongzhang931@gmail.com>
2016-09-28 21:25:51 -0400

    Added projects section and fix errors in work section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,72 +2,6 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-// var bio = {
-// 	"name" : "Chenglong Zhang",
-// 	"role" : "Web Developer",
-// 	"mobile" : "5144311188",
-// 	"email" : "chenglongzhang931@gmail.com",
-// 	"github" : "github.com/joeyzhang1989",
-// 	"location" : "Montreal, Canada",
-// 	"picture" : "images/me.jpeg",
-// 	"message" : "Thank your for your time reading my resume",
-// 	"skills" : ["Web Development", "Programming", "HTML", "CSS", "JavaScript", "Java",
-// 			  "GruntJS", "Gulp.js" ]
-// };
-
-
-// bio.name = HTMLheaderName.replace("%data%",bio.name);
-// bio.role = HTMLheaderRole.replace("%data%",bio.role);
-// bio.mobile = HTMLmobile.replace("%data%",bio.mobile);
-// bio.email = HTMLemail.replace("%data%",bio.email);
-// bio.github = HTMLgithub.replace("%data%",bio.github);
-// bio.location = HTMLlocation.replace("%data%",bio.location);
-// bio.picture = HTMLbioPic.replace("%data%",bio.picture);
-// bio.message = HTMLwelcomeMsg.replace("%data%",bio.message);
-
-// $("#header").prepend(bio.role);
-// $("#header").prepend(bio.name);
-// $("#topContacts").append(bio.mobile);
-// $("#topContacts").append(bio.email);
-// $("#topContacts").append(bio.github);
-// $("#topContacts").append(bio.location);
-// $("#header").append(bio.picture);
-// $("#header").append(bio.message);
-// $("#header").append(HTMLskillsStart);
-
-// for (var i = 0; i< bio.skills.length; i++) {
-// 	var id = bio.skills[i];
-// 	id = HTMLskills.replace("%data%",id);
-// 	$("#skills").append(id);
-// }
-
-
-// var work = {};
-// // var education = {};
-
-// // // workExperience
-// work["start"] = "February,2016";
-// work["employer"] = "The International Civil Aviation Organization";
-// work["title"] = "Statistical Data Analysis Intern";
-// work["dates"] = "February,2016-July,2016";
-// work["location"] = "Montreal, Canada";
-// work["description"] =   "Design and implement the fornt-end web components. Colsely worked with the back-end developers to optimize web layout";
-
-// work["start"] = HTMLworkStart.replace("%data%",work["start"]);
-// work["employer"] = HTMLworkEmployer.replace("%data%",work["employer"]);
-// work["title"] = HTMLworkTitle.replace("%data%",work["title"]);
-// work["dates"] = HTMLworkDates.replace("%data%",work["dates"]);
-// work["location"] = HTMLworkLocation.replace("%data%",work["location"]);
-// work["description"] = HTMLworkDescription.replace("%data%",work["description"]);
-
-
-// $("#workExperience").append(work["start"]);
-// $("#workExperience").append(work["employer"]);
-// $("#workExperience").append(work["title"]);
-// $("#workExperience").append(work["dates"]);
-// $("#workExperience").append(work["location"]);
-// $("#workExperience").append(work["description"]);
-
 // // education master
 // education.start = "Sept,2013";
 // education.name = "Concordia University";
@@ -181,19 +115,26 @@ var projects = {
 	"project": [
 	{
 		"start":"" ,
-		"title": "",
-		"dates": "",
-		"description":"" ,
-		"images":[""] 
+		"title": "Udacity-Front-End Nanodegree",
+		"dates": "Sept,2016",
+		"description":"<br>•Communicated with the client for the requirement of the website<br>•Structured the layout of the website <br>•Implemented website using HTML 5, CSS3, JS for content and web structure and the bootstrap for responsive design<br>•Deploy the website and test the visibility and stability of the website compatibility on multiple devices" ,
+		"images":["images/Udacity-Front-End-400.png"] 
 	},
 	{
-		"start":"" ,
-		"title": "",
-		"dates": "",
-		"description":"" ,
-		"images": [""]
+		"start":" " ,
+		"title": "Tower Defense Game",
+		"dates": "Sept,2014",
+		"description":"•Used GitHub to communicate and monitor the development<br>•Applied and modified MVC structure <br>•Programmed and designed the Map function<br>•Constructed unit tests by exploiting the Junit frameworkAdopted design patterns (strategy, observer, singleton, etc.)",
+		"images":["images/tower-defense-400.png"] 
+	},
+	{
+		"start":" " ,
+		"title": "File transfer System",
+		"dates": "Sept,2014",
+		"description":"<br>•Integrated winsocket to pack the data to transfer files between two Internet terminals <br>•Simulated the real-time communication by considering the loss and delay of the packets<br>•Deployed TCP and UDP protocol in different versions of program<br>•Validated the performance and stability of program by transferring various size and types of files ",
+		"images":["images/file-transfer-program-400.png"] 
 	}
-	]
+ ]
 }
 
 var displayBio = function () 
@@ -240,15 +181,35 @@ var displayWork = function()
 	// append in the HTML
 	$("#workExperience").append(work.experience[x].start);
 	$(".work-entry:last").append(formatedEmployerTitle);
-	$("#workExperience").append(work.experience[x].dates);
-	$("#workExperience").append(work.experience[x].location);
-	$("#workExperience").append(work.experience[x].description);
+	$(".work-entry:last").append(work.experience[x].dates);
+	$(".work-entry:last").append(work.experience[x].location);
+	$(".work-entry:last").append(work.experience[x].description);
  } 
+}
+
+projects.display = function () {
+	for (var x = 0; x < projects.project.length; x++) {
+	// replace the data
+	projects.project[x].start = HTMLprojectStart.replace("%data%",projects.project[x].start);
+	projects.project[x].title= HTMLprojectTitle.replace("%data%",projects.project[x].title);
+	projects.project[x].dates = HTMLprojectDates.replace("%data%",projects.project[x].dates);
+	projects.project[x].description = HTMLprojectDescription.replace("%data%",projects.project[x].description);
+	projects.project[x].images[0] = HTMLprojectImage.replace("%data%",projects.project[x].images[0]);
+	console.log(projects.project[x].images[x]);
+	// append in the HTML
+	$("#projects").append(projects.project[x].start);
+	$(".project-entry:last").append(projects.project[x].title);
+	$(".project-entry:last").append(projects.project[x].dates);
+	$(".project-entry:last").append(projects.project[x].description);
+	$(".project-entry:last").append(projects.project[x].images[0]);
+ } 	
 }
 
 displayBio();
 
 displayWork();
+
+projects.display();
 
 function inName(name) {
 	name = name.trim().split(" ");
@@ -258,3 +219,4 @@ function inName(name) {
 }
 
 $("#main").append(internationalizeButton);
+
