c74c6cc4acc47c24057016b39f8b3f844d892547
joeyzhang1989 <joeyzhang1989@users.noreply.github.com>
2023-06-07 20:55:04 +0000

    deploy: 74fc2ce608af29c8e1dcc94241f103ef00184701

---
+++ _next/static/5NuMbZe9qJA8-oVGR4Zh4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4858de22d0749d3b.css","static/chunks/pages/index-d822a93559cffd6d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
