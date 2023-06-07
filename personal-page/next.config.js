be5e361060f243fca1717a8fdb5cec59d7f27b69
Chenglong Zhang <chenglongzhang931@gmail.com>
2023-06-07 15:42:17 -0400

    add export in next  config

--- personal-page/next.config.js
+++ personal-page/next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  /**
-   * Tell Next.js where the `public` folder is.
-   * Replace `nextjs-github-pages` with your Github repo project name.
-   */
-  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
+const nextConfig = {
+  output: "export",
   /**
    * Disable server-based image optimization.
    *
@@ -15,4 +11,6 @@ module.exports = {
   images: {
     unoptimized: true,
   },
+  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
 };
+module.exports = nextConfig;
