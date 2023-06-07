fa7310eaa2e88accfa473922510c05d283bb7a99
joeyzhang1989 <joeyzhang1989@users.noreply.github.com>
2023-06-07 22:35:27 +0000

    deploy: 9478352f2c2dd1a1afb4917027f897c1831b2d62

--- _next/static/F6pd0r_NEF345rerUhhGZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4858de22d0749d3b.css","static/chunks/pages/index-d822a93559cffd6d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/I8ocXWzaapt41xL-X5vDB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6d9ce0b32e77ea5d.css","static/chunks/pages/index-18534862d81a65ba.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
